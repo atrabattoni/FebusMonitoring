255607289e3fc1f558045447864f68fba9719f7c
Alister Trabattoni <alister.trabattoni@gmail.com>
2021-07-27 14:14:32 +0200

    Add watcher example.

---
+++ examples/watcher.py
@@ -0,0 +1,15 @@
+import febus.cli as fc
+import febus.watcher as fw
+
+server = fc.launch()
+fc.enable()
+fc.start(2000, 1, 1, 24, 1000, 10, 80, "/home/febus/Pipelines/SR_writer.py")
+
+raw_state_updater = fw.RawStateUpdater("raw_state")
+state_updater = fw.StateUpdater("state")
+
+line = True
+while line:
+    line = server.stdout.readline().decode()
+    raw_state_updater.parse(line)
+    state_updater.parse(line)
