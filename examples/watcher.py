6e5bcd8b57b53751f2587ba196f67159394cb635
Alister Trabattoni <alister.trabattoni@gmail.com>
2021-07-28 17:00:46 +0200

    Other params.

--- examples/watcher.py
+++ examples/watcher.py
@@ -2,7 +2,7 @@ import febus.cli as fc
 import febus.watcher as fw
 
 server = fc.launch(gps=False)
-fc.start(4500, 2, 1, 30, 2000, 1, 40, "/home/febus/Pipelines/SR_writer.py")
+fc.start(4500, 1, 2, 30, 2000, 1, 40, "/home/febus/Pipelines/SR_writer.py")
 fc.enable()
 file_watcher = fw.FileWatcher()
 terminal_watcher = fw.TerminalWatcher(
