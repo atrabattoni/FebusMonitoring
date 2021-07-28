f73046620666177ac9d597db9c5cac2e19240d54
Alister Trabattoni <alister.trabattoni@gmail.com>
2021-07-28 14:30:16 +0200

    Declare watcher at the end

--- examples/watcher.py
+++ examples/watcher.py
@@ -2,7 +2,7 @@ import febus.cli as fc
 import febus.watcher as fw
 
 server = fc.launch(gps=False)
-watcher = fw.Watcher(server, "state", "raw_state")
 fc.start(2000, 1, 1, 24, 1000, 10, 80, "/home/febus/Pipelines/SR_writer.py")
 fc.enable()
+watcher = fw.Watcher(server, "state", "raw_state")
 watcher.run()
