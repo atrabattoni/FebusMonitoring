97ec7c7936a9cb987ef22cced9cce3d0eafe54ed
Alister Trabattoni <alister.trabattoni@gmail.com>
2021-07-28 16:51:25 +0200

    debug

--- examples/watcher.py
+++ examples/watcher.py
@@ -8,7 +8,7 @@ time.sleep(1)
 
 fc.start(2000, 1, 1, 24, 1000, 10, 80, "/home/febus/Pipelines/SR_writer.py")
 fc.enable()
-file_watcher = fw.FileWatcher()
-terminal_watcher = fw.TerminalWatcher([file_watcher], "state", "raw_state")
+# file_watcher = fw.FileWatcher()
+# terminal_watcher = fw.TerminalWatcher([file_watcher], "state", "raw_state")
 for line in sys.stdin:
-    terminal_watcher.parse(line)
+    print(line)
