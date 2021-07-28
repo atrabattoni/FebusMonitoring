6eb6cf1a9eb80995ee6457dae5a896c7c8b4e25b
Alister Trabattoni <alister.trabattoni@gmail.com>
2021-07-28 15:34:24 +0200

    Fix example.

--- examples/watcher.py
+++ examples/watcher.py
@@ -5,7 +5,7 @@ server = fc.launch(gps=False)
 fc.start(2000, 1, 1, 24, 1000, 10, 80, "/home/febus/Pipelines/SR_writer.py")
 fc.enable()
 file_watcher = fw.FileWatcher()
-terminal_watcher = fw.TerminalWatcher(server, file_watcher, "state", "raw_state")
+terminal_watcher = fw.TerminalWatcher(server, [file_watcher], "state", "raw_state")
 while True:
     line = server.stdout.readline()
     terminal_watcher.parse(line)
