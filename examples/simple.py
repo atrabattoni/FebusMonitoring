51d68f8d6ee241f6c0b67f32d6138b40bd78a131
Alister Trabattoni <alister.trabattoni@gmail.com>
2021-07-29 15:33:01 +0200

    Clean obsolete files.

--- examples/simple.py
+++
@@ -1,9 +0,0 @@
-from febus.cli import launch, start, enable
-from febus.monitor import chunk
-
-server = launch(gps=False)
-enable()
-start(2000, 1, 1, 24, 1000, 10, 80, "/home/febus/Pipelines/SR_writer.py")
-
-while True:
-    print(server.stdout.readline(), end="", flush=True)
