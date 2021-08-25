a7ee9564a497ca7220fb5c4c45d157f400c63949
Alister Trabattoni <alister.trabattoni@gmail.com>
2021-08-25 12:30:58 +0200

    Remove error on exit.

--- febus/cli.py
+++ febus/cli.py
@@ -38,7 +38,6 @@ class FebusDevice:
             preexec_fn=os.setsid,
         )
         self.server.stdout.reconfigure(line_buffering=True, write_through=True)
-        atexit.register(self.__del__)
         time.sleep(1)
         print(f"Server Started {'with GPS' if self.gps else 'without GPS'}")
 
