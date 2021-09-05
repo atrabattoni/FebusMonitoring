a822b59f8f9ab6fb713533ad09e3f68eab067e94
Alister Trabattoni <alister.trabattoni@gmail.com>
2021-09-05 13:57:06 +0200

    Add data_processor OSError handling.

--- templates/data_processor.py
+++ templates/data_processor.py
@@ -7,8 +7,11 @@ import shutil
 def data_processor(fname):
     warnings.filterwarnings("ignore")
     drive = pathlib.Path("/run/media/febus/Elements")
-    xarr = daspy.io.read(fname)
-    xarr = daspy.io.trim(xarr)
-    xarr.to_netcdf(drive / fname.with_suffix(".nc"))
-    shutil.move(fname.with_suffix(".log"), drive / fname.with_suffix(".log"))
-    fname.unlink()
+    try:
+        xarr = daspy.io.read(fname)
+        xarr = daspy.io.trim(xarr)
+        xarr.to_netcdf(drive / fname.with_suffix(".nc"))
+        shutil.move(fname.with_suffix(".log"), drive / fname.with_suffix(".log"))
+        fname.unlink()
+    except OSError as error:
+        print(error)
